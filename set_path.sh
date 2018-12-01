84f5b72774bf1697deadf6741910c0b7e0c3c3c5
darenlee <daren.lee@mathworks.com>
2018-12-01 16:25:30 -0500

    updated set_path.sh script

--- set_path.sh
+++ set_path.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-export HOME=/local/petalinux_projects/ZCU111_RFSoC/rdf0476-zcu111-rf-dc-eval-tool-2018-2/apu/rfsoc_petalinux_bsp/tmp_home/
-source /local/petalinux/settings.sh
+ource /local/petalinux/settings.sh
