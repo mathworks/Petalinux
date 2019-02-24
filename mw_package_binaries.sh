6b294a229c81d1d34b54e30d0d999e3c453dadf6
Daren Lee <darenlee@darenlee-deb9-64>
2019-02-24 17:00:25 -0500

    mw_package_binaries.sh
    Updated folder path to point to new mw_build_utils

--- mw_package_binaries.sh
+++ mw_package_binaries.sh
@@ -8,7 +8,7 @@ timestamp_sdcard() {
 }
 
 build/tmp/sysroots-components/x86_64/u-boot-mkimage-native/usr/bin/mkimage -A arm64 -T ramdisk -C gzip -d images/linux/rootfs.cpio.gz images/uramdisk.image.gz
-petalinux-package --boot --format BIN --bif ./images/boot.bif -o ./images/BOOT.BIN --force
+petalinux-package --boot --format BIN --bif ./mw_build_utils/boot.bif -o ./images/BOOT.BIN --force
 
 src_dir="$(pwd)"
 get_timestamp=$(timestamp)
@@ -21,7 +21,7 @@ cp_cmd2="cp $src_dir/images/uramdisk.image.gz $dst_dir"
 cp_cmd3="cp $src_dir/images/linux/system.dtb $dst_dir/devicetree.dtb"
 cp_cmd4="cp $src_dir/images/linux/Image $dst_dir"
 cp_cmd5="cp $src_dir/images/linux/system.bit $dst_dir"
-cp_cmd6="cp $src_dir/images/interfaces $dst_dir"
+cp_cmd6="cp $src_dir/mw_build_utils/interfaces $dst_dir"
 
 $mkdir_cmd; $cp_cmd1; $cp_cmd2; $cp_cmd3; $cp_cmd4; $cp_cmd5; $cp_cmd6
 
