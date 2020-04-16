4ec716ada889c0bbec648771a98124037b6dcd36
darenlee <daren.lee@mathworks.com>
2020-04-16 18:04:49 -0400

    mw_package_binaries.sh updated DTB file generation updated to include new shared-mem nodes

--- mw_package_binaries.sh
+++ mw_package_binaries.sh
@@ -20,12 +20,12 @@ assemble_dtb() {
     DTS_BASE=$9    # system-top.dtsi file located in components
     DST_DIR=${10}  # output dir location for all binaries
     OUTPUT_NAME=${11}  # prefix of user-specified DTB name, ie: devicetree or devicetree_axi4master
-
+    USE_SHAREDMEM=${12:-0}
     PP_OUTPUT=`basename ${DTS_BASE}`_${OUTPUT_NAME}_${DMAWIDTH}.pp
     DTS_NAME=`basename -s .dts ${DTS_BASE}`_${OUTPUT_NAME}_${DMAWIDTH}
 
   
-    gcc  -E -nostdinc -Ulinux -x assembler-with-cpp -DMW_DATAWIDTH_SELECT=${DMAWIDTH} -DMW_SAMPLECNT_REG=${DMASAMPLECNT} -I${DTS_BASE_DIR} -I${DTSI_FILES} -I${MW_DTSI_FILES} -I${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/include -I${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/arch/arm64/boot/dts/xilinx -I${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/include -I${XIL_DTS_DIR} -o ${OUTPUT_DIR}${PP_OUTPUT} ${DTS_BASE}
+    gcc  -E -nostdinc -Ulinux -x assembler-with-cpp -DMW_SHAREDMEM_ENABLE=${USE_SHAREDMEM} -DMW_DATAWIDTH_SELECT=${DMAWIDTH} -DMW_SAMPLECNT_REG=${DMASAMPLECNT} -I${DTS_BASE_DIR} -I${DTSI_FILES} -I${MW_DTSI_FILES} -I${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/include -I${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/arch/arm64/boot/dts/xilinx -I${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/include -I${XIL_DTS_DIR} -o ${OUTPUT_DIR}${PP_OUTPUT} ${DTS_BASE}
     dtc -I dts -O dtb -R 8 -p 0x1000 -b 0 -i ${DTS_BASE_DIR} -i ${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/include -i ${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/arch/arm64/boot/dts/xilinx -i ${BUILD_BASE_DIR}/work-shared/zcu111-zynqmp/kernel-source/include -i ${XIL_DTS_DIR} -o ${OUTPUT_DIR}${DTS_NAME}.dtb ${OUTPUT_DIR}${PP_OUTPUT}
     # Convert to DTS for examining
     dtc -I dtb -O dts -o ${OUTPUT_DIR}/${DTS_NAME}.dts ${OUTPUT_DIR}/${DTS_NAME}.dtb
@@ -74,8 +74,9 @@ MW_DTSI_FILES="$(pwd)/project-spec/meta-user/recipes-bsp/device-tree/files/mathw
 
 echo "XIL DTS Dir = $XIL_DTS_DIR"
 
-assemble_dtb 64 64 ${DTS_BASE_DIR} ${DTSI_FILES} ${MW_DTSI_FILES} ${BUILD_BASE_DIR} ${XIL_DTS_DIR} ${OUTPUT_DIR} ${DTS_BASE} ${dst_dir} devicetree_axi4master
-assemble_dtb 128 64 ${DTS_BASE_DIR} ${DTSI_FILES} ${MW_DTSI_FILES} ${BUILD_BASE_DIR} ${XIL_DTS_DIR} ${OUTPUT_DIR} ${DTS_BASE} ${dst_dir} devicetree_axi4master
+# Sample count register adjusted for axi4-master dtb files
+assemble_dtb 64 64 ${DTS_BASE_DIR} ${DTSI_FILES} ${MW_DTSI_FILES} ${BUILD_BASE_DIR} ${XIL_DTS_DIR} ${OUTPUT_DIR} ${DTS_BASE} ${dst_dir} devicetree_axi4master 1
+assemble_dtb 128 64 ${DTS_BASE_DIR} ${DTSI_FILES} ${MW_DTSI_FILES} ${BUILD_BASE_DIR} ${XIL_DTS_DIR} ${OUTPUT_DIR} ${DTS_BASE} ${dst_dir} devicetree_axi4master 1
 assemble_dtb 128 8 ${DTS_BASE_DIR} ${DTSI_FILES} ${MW_DTSI_FILES} ${BUILD_BASE_DIR} ${XIL_DTS_DIR} ${OUTPUT_DIR} ${DTS_BASE} ${dst_dir} devicetree
 
 # Create .zip file
